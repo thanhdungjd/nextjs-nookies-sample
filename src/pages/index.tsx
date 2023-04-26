50a85b232df0777c9711bfe4be42e051ce9961f0
Alex Le <dunglt@innotech-vn.com>
2023-04-26 17:26:07 +0700

    save

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,37 @@
-export default function Home() {
-  return <main className="p-5">CLEAN</main>;
-}
+import { useState, useEffect } from 'react';
+import { parseCookies, setCookie } from 'nookies';
+
+const Toolbar = ({ visible, onHide }) => {
+  return (
+    <div style={{ display: visible ? 'block' : 'none' }}>
+      <p>This is a toolbar!</p>
+      <button onClick={onHide}>Hide for 2 hours</button>
+    </div>
+  );
+};
+
+const Home = () => {
+  const [showToolbar, setShowToolbar] = useState(true);
+
+  useEffect(() => {
+    const cookies = parseCookies();
+    const showToolbarCookie = cookies['showToolbar'];
+    if (showToolbarCookie === 'false') {
+      setShowToolbar(false);
+    }
+  }, []);
+
+  const handleHideToolbar = () => {
+    setShowToolbar(false);
+    setCookie(null, 'showToolbar', 'false', { maxAge: 7200 });
+  };
+
+  return (
+    <div>
+      <Toolbar visible={showToolbar} onHide={handleHideToolbar} />
+      <p>Some content here</p>
+    </div>
+  );
+};
+
+export default Home;
